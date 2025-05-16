fe758f403568ce9dba21eee4ef811b5f83cce610
Gone Mallikarjun <gmallik1011@gmail.com>
2025-05-16 05:43:55 -0700

    Update build script to upgrade pip

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip first
+python -m pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
